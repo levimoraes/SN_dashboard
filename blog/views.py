d171d6a8b7f07d35be6b4982593ebe1c46e08b68
Levi Moraes <levi.moraes@yahoo.com.br>
2018-08-22 07:56:39 -0300

    Template Site

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,4 @@ from .models import Post, Candidato
 def post_list(request):
     candidato = Candidato.objects.all() 
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/index.html', {'posts': posts})
